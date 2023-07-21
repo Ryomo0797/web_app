5baf2af5902a065f12103959f1f1d61604283cd8
Ryomo0797 <s2222019@stu.musashino-u.ac.jp>
2023-07-21 16:16:44 +0900

    add

--- app_test.py
+++ app_test.py
@@ -36,7 +36,7 @@ if data is not None:
   # 地図オブジェクトの作成
   map = folium.Map(tiles='OpenStreetMap')
   #color_list=['red','blue','green','purple','orange','darkred','lightred','beige','darkblue','darkgreen','cadetblue','darkpurple','white','pink','lightblue','lightgreen','gray','black','lightgray']
-  color_list=["darkred"]
+  color_list=["blue"]
   # 各人物の軌跡を追加
   for idx, person in enumerate(person_list):
     data_temp = data[data["person"] == person]
